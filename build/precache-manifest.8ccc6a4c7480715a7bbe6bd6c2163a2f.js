bdd4bdd9c256741dae41ec80a264a82b117bcf89
Abror <47494166+abrorsaidov@users.noreply.github.com>
2020-10-23 23:34:05 +0300

    Add files via upload

---
+++ build/precache-manifest.8ccc6a4c7480715a7bbe6bd6c2163a2f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf864d23363a8a59dc6dd8b60ec3ab3f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7973ee9ecbdb030e842a",
+    "url": "/static/css/main.1aedf7d0.chunk.css"
+  },
+  {
+    "revision": "87742121cef56e368340",
+    "url": "/static/js/2.b0ed7b3c.chunk.js"
+  },
+  {
+    "revision": "320addaff3e37295a01ab28fa4b66477",
+    "url": "/static/js/2.b0ed7b3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7973ee9ecbdb030e842a",
+    "url": "/static/js/main.2f4de78e.chunk.js"
+  },
+  {
+    "revision": "b27a1eb3e0535f6c9e6d",
+    "url": "/static/js/runtime-main.73ac78b7.js"
+  },
+  {
+    "revision": "006f9d1efcfba5db0287a8583822e976",
+    "url": "/static/media/plastinka.006f9d1e.jpg"
+  }
+]);
